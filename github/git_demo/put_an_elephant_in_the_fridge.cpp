57205917aa386ac25ae93e87da0a1c3d04b25acd
ljy <1307856588@qq.com>
2025-07-05 16:37:37 +0800

    第一次提交

--- github/git_demo/put_an_elephant_in_the_fridge.cpp
+++ github/git_demo/put_an_elephant_in_the_fridge.cpp
@@ -4,15 +4,18 @@ using namespace std;
 void open_the_fridge() {
     // add code here
    // user A
+   std::cout << "感谢大家-open-hhh" << std::endl;
 }
 void put_the_elephant_in() {
     // add code here
     // user B
+    std::cout << "感谢大家-put" << std::endl;
 }
 
 void close_the_fridge() {
     // add code here
     // user B
+    std::cout << "感谢大家-close" << std::endl;
 }
 
 void warning() {
